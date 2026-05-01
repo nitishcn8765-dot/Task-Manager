f6ad8a7e113b93f49994adceb5469f216ece6b4e
nitishcn8765-dot <nitishcn8765@gmail.com>
2026-05-01 17:35:13 +0530

    fix project structure

--- backend/routes/projectRoutes.js
+++ backend/routes/projectRoutes.js
@@ -1,13 +1,18 @@
-const mongoose = require("mongoose");
+const express = require("express");
+const router = express.Router();
 
-const projectSchema = new mongoose.Schema({
-  name: String,
-  description: String,
-  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  members: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }]
-});
-const { addMember } = require("../controllers/projectController");
+const {
+  createProject,
+  addMember
+} = require("../controllers/projectController");
 
+const auth = require("../middleware/authMiddleware");
+const role = require("../middleware/roleMiddleware");
+
+// Create project
+router.post("/create", auth, role(["Admin"]), createProject);
+
+// Add member
 router.post("/add-member", auth, role(["Admin"]), addMember);
 
-module.exports = mongoose.model("Project", projectSchema);
+module.exports = router;
