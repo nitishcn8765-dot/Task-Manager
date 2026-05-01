dd451350a8d8e7adcc99322d534a43e0d501b86a
nitishcn8765-dot <nitishcn8765@gmail.com>
2026-05-01 17:50:54 +0530

    improved UI flow

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -5,4 +5,9 @@ const { signup, login } = require("../controllers/authController");
 router.post("/signup", signup);
 router.post("/login", login);
 
+router.get("/get-user", async (req, res) => {
+  const user = await User.findOne({ email: req.query.email });
+  res.json(user);
+});
+
 module.exports = router;
